b105c96356965a5464cff26929de51f5681c6060
Ygor Tavares <143301541+Ygoortg@users.noreply.github.com>
2026-06-08 21:26:04 -0300

    Update ProdutoCRUD.java

--- ProdutoCRUD.java
+++ ProdutoCRUD.java
@@ -1,12 +1,12 @@
 // Criando interface
 //Todos abaixo são métodos
-//Usamos boolean para true or false
+//Usamos boolean para informar se deu certo ou errado
 
-public interface ProdutoCRUD {
+public interface ProdutoCRUD{
     public boolean inserir(Produto produto);//Adiciona um novo produto no array.
     public Produto pesquisar(Produto produto);//Procura um produto pelo ID
     public boolean remover(int id);//Remove um produto do array.
     public boolean atualizarNome(int id, String novoValor);// Altera nome do produto
-    public boolean atualizarPreco(int id, double preco);//Atualiza preço
-
+    boolean atualizarPreco(int id, double preco);//Atualiza preço]
+    Produto[] listarProdutos();
 }
